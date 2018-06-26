f6fde8b599fdca5609f2647b62179981e65e2373
Jennifer Kulich <kjen9986@gmail.com>
2018-06-26 14:33:08 -0500

    prog2- asked user for guess

--- Prog2/Prog2.cpp
+++ Prog2/Prog2.cpp
@@ -7,6 +7,8 @@ int main()
 	int board[10][10] = { 0 };
 	int randomXSpot = 0;
 	int randomYSpot = 0;
+	int guessX = 0;
+	int guessY = 0;
 
 	//for loop that will get random spot for all 3 ships
 	int i;
@@ -21,6 +23,26 @@ int main()
 		//mark random spot of ship
 		board[randomXSpot - 1][randomYSpot - 1] = i;
 	}
+
+	//ask user for a spot
+	cout << "Enter shot row and column (1-10)" << endl;
+	cout << "Row: ";
+	cin >> guessX;
+	while (guessX < 1 || guessX > 10)
+	{
+		cout << endl << "Row must be between 1 and 10. Please enter another row: ";
+		cin >> guessX;
+		cout << endl;
+	}	
+	cout << endl << "Column: ";
+	cin >> guessY;
+	while (guessY < 1 || guessY > 10)
+	{
+		cout << endl << "Column must be between 1 and 10. Please enter another Column: ";
+		cin >> guessY;
+		cout << endl;
+	}
+	cout << endl;
 
 
 	//check to make sure board spot changes
