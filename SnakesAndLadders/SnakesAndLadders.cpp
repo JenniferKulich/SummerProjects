a8b4b3724a2f9ce1c0ec9543a4d0a434fc57d2c7
Jennifer Kulich <kjen9986@gmail.com>
2018-07-09 12:55:18 -0500

    SnakesAndLadders- have the function for roll or quit written

--- SnakesAndLadders/SnakesAndLadders.cpp
+++ SnakesAndLadders/SnakesAndLadders.cpp
@@ -2,10 +2,13 @@
 
 using namespace std;
 
+int rollOrQuit();
+
 int main()
 {
 	int board[5][8] = { 0 };
 	int numPlayers = 0;
+	int answerContinue = 0;
 
 
 	//ask user for number of players
@@ -24,6 +27,36 @@ int main()
 	}
 
 
+	//ask the user if they would like to roll or quit
+	answerContinue = rollOrQuit();
+	//if neither roll or quit were selected, ask again until it is
+	while (answerContinue == 5)
+	{
+		cout << "Invalid choice. Please enter R for roll or Q for quit" << endl;
+		answerContinue = rollOrQuit();
+	}
+	//if quit was picked, quit the program
+	if (answerContinue == 4)
+		return 0;
+	
+		
+
+
+
+
 
 	return 0;
+}
+
+int rollOrQuit()
+{
+	char choice;
+	cout << "Would you like to roll(R) or quit(Q)? ";
+	cin >> choice;
+	if (choice == 'R' || choice == 'r')
+		return 3;
+	else if (choice == 'Q' || choice == 'q')
+		return 4;
+	else
+		return 5;
 }
