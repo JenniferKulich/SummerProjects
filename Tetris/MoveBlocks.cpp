dfb9d70b7b445d3efd48919c3b9f17a670c75092
Jennifer Kulich <kjen9986@gmail.com>
2018-08-01 20:51:16 -0500

    tetris- have left and right movement for vertical I block

--- Tetris/MoveBlocks.cpp
+++ Tetris/MoveBlocks.cpp
@@ -3,11 +3,11 @@
 //3 is RIGHT
 //1 is LEFT
 
-//function that will move the horizontal I block down, left, or right
+
+//function that will move the horizontal I block left, or right
 void moveHorizontalIBlock(char boardColors[][10], char colorName, int &startSpot, int userMove, int row)
 {
 	//start spot is the very left block
-	int i;
 
 	//if the user move is 3, the block will go to the right once
 	if (userMove == 3)
@@ -34,6 +34,50 @@ void moveHorizontalIBlock(char boardColors[][10], char colorName, int &startSpot
 		}
 
 	}
+
 	
+	return;
+}
+
+
+//function that will move the vertical I block left, or right
+void moveVerticalIBlock(char boardColors[][10], char colorName, int &startSpot, int userMove, int row)
+{
+	//start spot is the very top block
+	int i;
+
+	//if user move is 3, check to move all to the right
+	if (userMove == 3)
+	{
+		if (startSpot < 9)
+		{
+			//loop through to move all over
+			for (i = row; i < row + 3; i++)
+			{
+				boardColors[i][startSpot] = 00;
+				boardColors[i][startSpot + 1] = colorName;
+				startSpot = startSpot + 1;
+
+			}
+		}
+	}
+
+
+	//if user move is 1, check to move all to the left
+	if (userMove == 1)
+	{
+		if (startSpot > 0)
+		{
+			//loop through to move all over
+			for (i = row; i < row + 3; i++)
+			{
+				boardColors[i][startSpot] = 00;
+				boardColors[i][startSpot - 1] = colorName;
+				startSpot = startSpot - 1;
+
+			}
+		}
+	}
+
 	return;
 }
--- Tetris/MoveBlocks.h
+++ Tetris/MoveBlocks.h
@@ -2,3 +2,4 @@
 
 
 void moveHorizontalIBlock(char boardColors[][10], char colorName, int &startSpot, int userMove, int row);
+void moveVerticalIBlock(char boardColors[][10], char colorName, int &startSpot, int userMove, int row);
